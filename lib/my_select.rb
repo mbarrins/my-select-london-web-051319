d1f15c9a8d1b7f154bb7693cfe5443e495d58734
Maija Barrins <maija.barrins@gmail.com>
2019-04-26 16:00:24 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    new_collection = []
+    while i < collection.length
+      new_collection << collection[i] if yield(collection[i])
+      i = i + 1
+    end
+    new_collection
+  else
+    "This block should not run!"
+  end
 end
